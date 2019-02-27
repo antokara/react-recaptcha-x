7876ca2a3fd000c7a8b3e7e631d83412223aa7b8
Antonios Karagiannis <antokarag@gmail.com>
2019-02-26 21:42:16 -0500

    add build script for module

---
+++ src/module/webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'ReactReCaptchaV3V2.tsx'),
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  output: {
+    path: path.resolve(__dirname, '..', '..', 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|assets|reports)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  { targets: { browsers: ['> 5%', 'last 2 versions'] } }
+                ],
+                '@babel/preset-typescript',
+                '@babel/preset-react'
+              ],
+              plugins: ['@babel/plugin-proposal-object-rest-spread']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  externals: {
+    react: 'commonjs react'
+  }
+};
