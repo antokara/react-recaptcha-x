a749d43004f098aacffa3455395df36e0896058b
Antonios Karagiannis <antokarag@gmail.com>
2019-06-15 12:02:06 -0400

    fix Context creation

--- module/src/provider/Context.tsx
+++ module/src/provider/Context.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import { IContext } from './IContext';
 
 /**
- * the Provider's context
+ * create the Provider's context
+ * with default values
  */
-const Context: React.Context<IContext> = React.createContext<IContext | null>(
-  undefined
-);
+const Context: React.Context<IContext> = React.createContext<IContext>({
+  siteKeyV3: '',
+  loaded: false
+});
 
 export { Context };
