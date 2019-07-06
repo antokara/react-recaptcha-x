4fd709c0952043475c6b308df8e5aa01f3cf1eca
Antonios Karagiannis <antokarag@gmail.com>
2019-07-05 23:34:28 -0400

    test(jest config): specify coverage
    * set coverage configuration
    * allow tslint to let packages be in devDependencies

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,16 @@ const { compilerOptions } = require('./tsconfig');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  testURL: 'http://localhost',
+  coverageDirectory: '<rootDir>/reports/coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 };
