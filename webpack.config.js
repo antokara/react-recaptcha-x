160e40b648b300bd75bf7080d2afcd015d5d5bb8
Antonios Karagiannis <antokarag@gmail.com>
2019-07-07 16:56:56 -0400

    build(webpack): replace babel with tsc
    * remove all babel packages and babel-loader
    * add ts-loader package
    * add ts:validate npm script to compile/validate the project
    * specify TS typeRoots for grecaptcha global d.ts
    * replace webpack babel-loader config with ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,21 +17,10 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        exclude: /(node_modules|assets|reports)/,
+        exclude: /(node_modules|reports)/,
         use: [
           {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env',
-                  { targets: { browsers: ['> 5%', 'last 2 versions'] } }
-                ],
-                '@babel/preset-typescript',
-                '@babel/preset-react'
-              ],
-              plugins: ['@babel/plugin-proposal-object-rest-spread']
-            }
+            loader: 'ts-loader'
           }
         ]
       }
