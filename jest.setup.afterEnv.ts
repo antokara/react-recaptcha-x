cd9898f1c237d7a91c98028e2765ca008df17a04
Antonios Karagiannis <antokarag@gmail.com>
2019-10-06 22:20:16 -0600

    build(jest ide): add Jest DOM custom matchers
    * convert jest setup file to .ts
    * add jest setup file after env that loads the custom matchers
    * add tsconfig.build.json for the build script
    * change build scripts to use the tsconfig.build.json
    * modify tsconfig.json to import the custom matchers (for IDE syntax)
    * add tsconfig.readme.txt for explanation / future notes
    * change ts-lodaer inwebpack.config.js to use tsconfig.build.json

---
+++ jest.setup.afterEnv.ts
@@ -0,0 +1,4 @@
+// enable Jest DOM custom matchers
+// @see https://github.com/testing-library/jest-dom#usage
+// tslint:disable-next-line:no-import-side-effect no-submodule-imports
+import '@testing-library/jest-dom/extend-expect';
