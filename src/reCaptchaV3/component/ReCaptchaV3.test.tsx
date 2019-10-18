b509cafafb0bf0bc62bc2c172fb31163afa09985
Antonios Karagiannis <antokarag@gmail.com>
2019-10-17 23:41:31 -0600

    test(recaptcha v3 component): add tests
    * increase file coverage to 100%

--- src/reCaptchaV3/component/ReCaptchaV3.test.tsx
+++ src/reCaptchaV3/component/ReCaptchaV3.test.tsx
@@ -5,7 +5,7 @@ import { ReCaptchaV3 } from './ReCaptchaV3';
 import { TCallback } from './TCallback';
 
 describe('ReCaptchaV3 component', () => {
-  let callback: TCallback;
+  let callback: jest.Mock<TCallback>;
   let providerContext: IContext;
   let rr: RenderResult;
   let node: ChildNode | null;
@@ -58,6 +58,54 @@ describe('ReCaptchaV3 component', () => {
 
       it('renders nothing', () => {
         expect(node).toBeFalsy();
+      });
+
+      describe('when providerContext.loaded changes to true', () => {
+        beforeEach(() => {
+          // make sure the mocked callback hasn't been called before
+          callback.mockClear();
+          // mock the google reCaptcha object
+          global.grecaptcha = {
+            render: jest.fn(),
+            reset: jest.fn(),
+            getResponse: jest.fn(),
+            // execute: jest.fn((widgetId?: number, options?: options): void => { return; })
+            execute: jest
+              .fn()
+              .mockImplementation(
+                (siteKey: string, options?: options): Promise<string> =>
+                  Promise.resolve('test-token')
+              )
+          };
+          // change loaded to true
+          providerContext = {
+            ...providerContext,
+            loaded: true
+          };
+          rr.rerender(
+            <ReCaptchaV3
+              action="test-action"
+              callback={callback}
+              providerContext={providerContext}
+            />
+          );
+        });
+
+        it('invokes the google reCaptcha execute once', () => {
+          expect(global.grecaptcha.execute).toHaveBeenCalledTimes(1);
+        });
+
+        it('invokes the callback twice', () => {
+          expect(callback).toHaveBeenCalledTimes(2);
+        });
+
+        it('invokes the callback without any arguments', () => {
+          expect(callback).toHaveBeenNthCalledWith(1);
+        });
+
+        it('invokes the callback with the token', () => {
+          expect(callback).toHaveBeenNthCalledWith(2, 'test-token');
+        });
       });
     });
   });
