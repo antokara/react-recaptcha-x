4997862bcf0d8e85de9242f4c7ec05fdc499fb19
Antonios Karagiannis <antokarag@gmail.com>
2019-10-20 12:45:34 -0600

    test(provider): refactor to smaller files
    * for tslint max file line count rule

---
+++ src/provider/ReCaptchaProvider.test/withRequiredProps.test.tsx
@@ -0,0 +1,107 @@
+import { getByTestId, render, RenderResult } from '@testing-library/react';
+import * as React from 'react';
+import { ReCaptchaProvider } from 'src/provider/ReCaptchaProvider';
+import { withContext } from 'src/provider/withContext';
+import { clearDOM } from './helpers/clearDOM';
+import { DummyComponent } from './helpers/DummyComponent';
+import { IProps } from './helpers/IProps';
+
+describe('ReCaptchaProvider', () => {
+  let rr: RenderResult;
+  let node: ChildNode | null;
+  let DummyComponentWithContext: React.ComponentType<IProps>;
+
+  describe('with required props', () => {
+    beforeEach(() => {
+      // make sure we clean DOM from script/style tags
+      clearDOM();
+      // wrap our dummy component with the context and get its props
+      DummyComponentWithContext = withContext(DummyComponent);
+      // render our dummy component in a two level nested node
+      // under the provider, to test the context passing down
+      rr = render(
+        <ReCaptchaProvider>
+          <div>
+            <DummyComponentWithContext dummy="dummy-prop" otherDummy={55} />
+          </div>
+        </ReCaptchaProvider>
+      );
+      node = getByTestId(rr.container, 'dummy-test-id');
+    });
+
+    describe('context provided props', () => {
+      it('does not have the siteKeyV2', () => {
+        expect(node).not.toHaveAttribute('data-sitekey-v2');
+      });
+
+      it('does not have the siteKeyV3', () => {
+        expect(node).not.toHaveAttribute('data-sitekey-v3');
+      });
+
+      it('has the loaded', () => {
+        expect(node).toHaveAttribute('data-loaded', 'false');
+      });
+    });
+
+    describe('own props', () => {
+      it('has the dummy prop', () => {
+        expect(node).toHaveAttribute('data-dummy', 'dummy-prop');
+      });
+
+      it('has the otherDummy prop', () => {
+        expect(node).toHaveAttribute('data-other-dummy', '55');
+      });
+    });
+
+    describe('Google reCAPTCHA JS API script tag', () => {
+      let scriptTagNode: ChildNode | null;
+      beforeEach(() => {
+        scriptTagNode = document.querySelector('script');
+      });
+
+      it('gets injected', () => {
+        expect(scriptTagNode).not.toBeNull();
+      });
+
+      it('gets injected only once', () => {
+        expect(document.querySelectorAll('script')).toHaveLength(1);
+      });
+
+      it('has the correct src attribute', () => {
+        expect(scriptTagNode).toHaveAttribute(
+          'src',
+          `https://www.google.com/recaptcha/api.js?render=explicit&onload=GoogleReCaptcha_onload&hl=`
+        );
+      });
+
+      it('has the async attribute set to true', () => {
+        expect(scriptTagNode).toHaveAttribute('async', 'true');
+      });
+
+      it('has the defer attribute set to true', () => {
+        expect(scriptTagNode).toHaveAttribute('defer', 'true');
+      });
+    });
+
+    describe('style tag for the Google reCAPTCHA V3 badge', () => {
+      let scriptTagNode: ChildNode | null;
+      beforeEach(() => {
+        scriptTagNode = document.querySelector('style');
+      });
+
+      it('does not get injected', () => {
+        expect(scriptTagNode).toBeNull();
+      });
+    });
+
+    describe('window.GoogleReCaptcha_onload callback', () => {
+      beforeEach(() => {
+        window.GoogleReCaptcha_onload();
+      });
+
+      it('the loaded prop changes to true', () => {
+        expect(node).toHaveAttribute('data-loaded', 'true');
+      });
+    });
+  });
+});
